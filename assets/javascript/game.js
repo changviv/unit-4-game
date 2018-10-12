a986919e1eefea73bb1f2903b6cd80ae4774121a
Vivien <vchang103@gmail.com>
2018-10-11 17:54:41 -0700

    remove comments and console log

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,4 @@
-//  The player will be shown a random number at the start of the game.
 var targetNumber = Math.floor(Math.random() * 101) + 19;
-// counter variable
 var totalScore = 0;
 var winCount = 0;
 var lossCount = 0;
@@ -10,7 +8,6 @@ var redCrystalValue = Math.floor(Math.random() * 12) + 1;
 var yellowCrystalValue = Math.floor(Math.random() * 12) + 1;
 var purpleCrystalValue = Math.floor(Math.random() * 12) + 1;
 
-// reset doesn't work when you win
 function resetGame() {
     targetNumber = Math.floor(Math.random() * 101) + 19;
     blueCrystalValue = Math.floor(Math.random() * 12) + 1;
@@ -24,14 +21,11 @@ function resetGame() {
     $("#random-number").text(targetNumber);
 }
 
-
-// fix setScore does not work
 function setScore(crystalValue) {
     totalScore += crystalValue
     $("#total-score").text(totalScore)
     if (totalScore === targetNumber) {
         winCount++;
-        alert("YOU WIN")
         resetGame();
         }
     else if (totalScore >= targetNumber) {
@@ -45,22 +39,17 @@ $(document).ready(function () {
 
     $(".blue").on("click", function() {
         setScore(blueCrystalValue);
-        console.log(blueCrystalValue)
     });
 
     $(".red").on("click", function() {
         setScore(redCrystalValue);
-        console.log(redCrystalValue)
     });
 
     $(".yellow").on("click", function() {
         setScore(yellowCrystalValue);
-        console.log(yellowCrystalValue)
     });
 
     $(".purple").on("click", function() {
         setScore(purpleCrystalValue);
-        console.log(purpleCrystalValue)
     });
-
 });
